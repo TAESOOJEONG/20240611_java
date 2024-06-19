cc14aafa5c193712da6b29c68012a777d9fd637b
TAESOOJEONG <jts7066@naver.com>
2024-06-19 15:42:30 +0900

    test

--- src/P03_method/Ex10while.java
+++ src/P03_method/Ex10while.java
@@ -1,51 +1,65 @@
-package P03_method;
+package p03_method;
 
 import javax.swing.*;
-import java.util.Scanner;
 
 public class Ex10while {
   public static void main(String[] args) {
-    int i = 2;
+    int i = 0;
+    int sum = 0;
+    while (i<10) {
+      sum += i++;
+    }
+    System.out.println(sum);
+
+    i = 2;
     int j = 1;
-    while (i < 10) {
-      j = 1;
-      while (j < 10) {
-        System.out.printf("%d x %d = %2d\n", i, j, i * j++);
+    while (j < 10) {
+      i = 2;
+      while (i < 10) {
+        System.out.printf("%d * %d = %2d \t", i, j, i++ * j);
       }
-      i++;
+      j++;
       System.out.println();
     }
-    i = 2;
 
+    System.out.println("=======내 마음의 숫자를 맞추기 게임=======");
+    int random = (int)(Math.random()*100)+1;
+    while (true) {
+      int answer = Integer.parseInt(JOptionPane.showInputDialog("숫자를 입력하세요(종료는q)"));
+      if (answer > random) {
+        System.out.println("작습니다.");
+      } else if (answer < random) {
+        System.out.println("큽니다");
+      } else {
+        System.out.println("정답입니다.");
+        break;
+      }
+    }
 
-    System.out.println("내 마음의 숫자를 맞춰봐(1~100)");
+    /*
+    System.out.println("=======내 마음의 숫자를 맞추기 게임=======");
     boolean stop = false;
     while (true) {
-      //Math.random() 0보다크고 1보다 작은 실수 ex 0.1~~ , 0.2~~, 0.7~~
       int random = (int) (Math.random() * 100) + 1;
       if (stop) break;
 
       while (true) {
-        String input = JOptionPane.showInputDialog("숫자를 입력하세요(종료는Q,q)");
+        String input = JOptionPane.showInputDialog("숫자를 입력하세요(종료하려면 Q,q)");
         if (input.toLowerCase().equals("q")) {
           stop = true;
           break;
-
         } else {
           int answer = Integer.parseInt(input);
-
           if (answer > random) {
-            System.out.println("그것 보다 작다.");
+            System.out.println("작습니다.");
           } else if (answer < random) {
-            System.out.println("그것 보다 큽니다.");
+            System.out.println("큽니다");
           } else {
-            System.out.println("정답.");
+            System.out.println("정답입니다.");
           }
-
-
         }
       }
     }
+    */
   }
 }
-
