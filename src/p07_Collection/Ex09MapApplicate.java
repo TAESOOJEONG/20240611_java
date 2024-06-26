9077f2e9787bbfdd78d230be34167e46ba65a4d5
TAESOOJEONG <jts7066@naver.com>
2024-06-26 14:58:40 +0900

    test

--- src/p07_Collection/Ex09MapApplicate.java
+++ src/p07_Collection/Ex09MapApplicate.java
@@ -1,11 +1,13 @@
 package p07_Collection;
 
+import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 public class Ex09MapApplicate {
   private static Map phoneBook = new HashMap();
+
   public static void main(String[] args) {
     addPhone("친구", "김수한", "010-1111-1110");
     addPhone("친구", "김수둘", "010-1111-1111");
@@ -17,28 +19,35 @@ public class Ex09MapApplicate {
 
   private static void addPhone(String group, String name, String tel) {
     if (!phoneBook.containsKey(group)) {
-      phoneBook.put(group,new HashMap<>());
+      phoneBook.put(group, new HashMap<>());
     }
     HashMap book = (HashMap) phoneBook.get(group);
     book.put(name, tel);
   }
-  private static void addPhone( String name, String tel) {
-    addPhone("기타",name,tel);
+
+  private static void addPhone(String name, String tel) {
+    addPhone("기타", name, tel);
   }
+
   private static void printBook(Map map) {
     Iterator it = phoneBook.keySet().iterator();
     while (it.hasNext()) {
       String group = (String) it.next();
       HashMap book = (HashMap) phoneBook.get(group);
-      System.out.printf("[ %s ] : %d개 \n", group,book.size());
-    Iterator subIt = phoneBook.entrySet().iterator();
-
+      System.out.printf("[ %s ] : %d개 \n", group, book.size());
+      Iterator subIt = book.entrySet().iterator();
+       while (subIt.hasNext()) {
+        Map.Entry entry = (Map.Entry) subIt.next();
+        System.out.printf(
+            "이름 : %s , 연락처 :%s \n", entry.getKey(), entry.getValue());
+      }
     }
   }
-
 }
 
-/*   친구  4개
+
+/*
+     친구  4개
     김수한 1110
     김수둘 1111
     김수셋 1112
@@ -49,4 +58,4 @@ public class Ex09MapApplicate {
     위 형식으로 출력
     key의 value.size = 4개 중첩반복문
 
-* */
+              * */
